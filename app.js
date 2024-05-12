d9bc93de47097787f32adc809f9be8f0cba738ff
someone <someone@someplace.com>
2024-05-12 10:56:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+import { dbConnection } from "./database/dbConnection.js";
+
+const app = express();
+// dotenv.config({ path: "./config.env" });
+dotenv.config();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTED_URL],
+    methods: ["POST"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/reservation", reservationRouter);
+app.get("/", (req, res, next)=>{return res.status(200).json({
+  success: true,
+  message: "HELLO WORLD AGAIN"
+})})
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
---
+++ database/dbConnection.js
@@ -0,0 +1,19 @@
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+export const dbConnection = () => {
+ 
+  mongoose.connect(process.env.MONGO_URI, {
+      dbName: "RESERVATIONS", useNewUrlParser: true, useUnifiedTopology: true }
+      
+      )
+    
+    .then(() => {
+      console.log("Connected to database!");
+      
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecing to database: ${err}`);
+    });
+};
