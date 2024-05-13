9bf5a340627c08dd093f7146dc5f6497d1ddd2a8
someone <someone@someplace.com>
2024-05-13 12:36:34 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 import app from "./app.js"
 
-const PORT = process.env.PORt || 4001;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
